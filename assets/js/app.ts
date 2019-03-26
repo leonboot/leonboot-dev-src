9c7c0b3c6558a5ca3af30a203d43fe99956c8f55
Leon Boot <leon@nedbase.nl>
2019-03-26 15:05:25 +0100

    Restructured DOM, added Fontawesome brand icons

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -1,1 +1,7 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
 require('../scss/app.scss');
+
+library.add(fab);
+dom.watch();
