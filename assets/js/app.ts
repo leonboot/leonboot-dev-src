5116361b686e88e2df96a4b6520aec8e9dc31a68
Leon Boot <leon@nedbase.nl>
2019-04-04 10:53:20 +0200

    Rating, Heroku configuration

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -17,6 +17,12 @@ document.querySelectorAll('a.ext').forEach(el => {
 
 document.querySelectorAll('.skills li').forEach(el => {
     if (el instanceof Element) {
+        if (el.firstChild instanceof Text) {
+            const titleNode = document.createElement('span');
+            titleNode.innerHTML = el.firstChild.textContent;
+            el.removeChild(el.firstChild);
+            el.appendChild(titleNode);
+        }
         const skill = {
             name: el.getAttribute('data-name'),
             proficiency: parseFloat(el.getAttribute('data-proficiency'))
@@ -24,5 +30,35 @@ document.querySelectorAll('.skills li').forEach(el => {
         const logo = document.createElement('img');
         logo.setAttribute('src', '/img/logos/'+skill.name+'.svg');
         el.insertBefore(logo, el.firstChild);
+        const rating = document.createElement('div');
+        rating.className = 'rating';
+        for (let i = 1; i <= 5; ++i) {
+            let star = document.createElement('span');
+            star.className = 'star-icon';
+            star.innerHTML = '&#9733;';
+            if (skill.proficiency > i) {
+                console.log(skill.name, skill.proficiency, i);
+                star.className += ' full';
+            } else if (skill.proficiency - i + 1 === 0.5) {
+                star.className += ' half';
+            }
+            rating.appendChild(star);
+        }
+        el.querySelector('span').appendChild(rating);
     }
-})
+});
+
+window.addEventListener('scroll', event => {
+    let lastElement: HTMLElement;
+    document.querySelectorAll('h2[id]').forEach((el: HTMLElement) => {
+        if (el.offsetTop - window.scrollY <= 0) {
+            lastElement = el;
+        }
+    });
+    if (lastElement instanceof HTMLElement) {
+        document.querySelectorAll('header nav .nav-link').forEach((el: HTMLElement) => {
+            el.classList.remove('active');
+        })
+        document.querySelector('header nav .nav-link[href="#'+lastElement.id+'"]').classList.add('active');
+    }
+});
